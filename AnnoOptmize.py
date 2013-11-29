99298871254c44f62c9a5787b2d2326db51f284e
Octeufer <octeufer@gmail.com>
2013-11-29 15:36:46 +0800

    build models completely leluch

--- AnnoOptmize.py
+++ AnnoOptmize.py
@@ -12,15 +12,15 @@ path = "d:/data/annooptimize/Annodata/"
 
 
 def rectp(rpoint,height,width):
-    height = float(height) / 1000 * 10000
-    width = float(width) / 1000 * 10000
+    height = float(height) / 1000 / 1000 * 10000
+    width = float(width) / 1000 / 1000 * 10000
     dx = width/2
     dy = height/2
     minx = rpoint[0]-dx
     miny = rpoint[1]-dy
     maxx = rpoint[0]+dx
     maxy = rpoint[1]+dy
-    return ((minx,maxy),(minx,miny),(maxx,miny),(maxx,maxy),(minx,maxy))
+    return [[[minx,maxy],[minx,miny],[maxx,miny],[maxx,maxy],[minx,maxy]]]
 
 class dataengine:
 
@@ -40,24 +40,33 @@ class dataengine:
         
         reader = shapefile.Reader(path + shpname + ".shp")
         fields = reader.fields
-        for sr in reader.shapeRecords():
+        for sr in reader.shapeRecords():           
             annopoi = dict(type="Feature",attributes=dict(), \
                            uniqueid=int(float(sr.record[8])),layername=shpname, \
                            geometry=sr.shape.__geo_interface__)
+            '''
+            annopoi = {"type": "Feature", \
+                       "geometry": { \
+                                    "type": "Point", \
+                                    "coordinates": list(sr.shape.__geo_interface__["coordinates"])}}
+            print annopoi
+            '''
+            annopoi["geometry"]["coordinates"] = list((annopoi["geometry"]["coordinates"][0] / 1000,annopoi["geometry"]["coordinates"][1] / 1000))
             self.db.annopois.insert(annopoi)
     
     def insertbarriergeo(self,barriername):
-        reader = shapefile.Reader(path + shpname + ".shp")
+        reader = shapefile.Reader(path + barriername + ".shp")
         for sr in reader.shapeRecords():
             barriergeo = dict(type="Feature",attributes=dict(), \
                               layername=barriername, \
                               geometry=sr.shape.__geo_interface__)
+            barriergeo["geometry"]["coordinates"] = [list(list((t[0]/1000,t[1]/1000)) for t in barriergeo["geometry"]["coordinates"])]
             self.db.barriergeo.insert(barriergeo)
     
     def insertpbarriergeo(self,plyrname):
         for p in self.db.annopois.find({"layername":plyrname}):
             pgeo = p["geometry"]["coordinates"]
-            polygeo = rectp(pgeo,3,3)
+            polygeo = dict(type="Polygon",coordinates=rectp(pgeo,3,3))
             newba = barrier(polygeo)
             newba.setlayername(plyrname)
             newba.setattributes(p["attributes"])
@@ -65,11 +74,20 @@ class dataengine:
     
     def insertlocgeo(self,annopoint):
         pgeo = annopoint.geometry["coordinates"]
-        polygeo = rectp(pgeo,3,15)
+        polygeo = dict(type="Polygon",coordinates=rectp(pgeo,3,15))
         locbarrier = barrier(polygeo)
         locbarrier.setlayername("locgeo")
         locbarrier.setattributes(annopoint.attributes)
         self.db.barriergeo.insert(locbarrier)
+        
+    def insertannobarrier(self,annopoly):
+        pgeo = dict(type="Polygon",coordinates=annopoly)
+        locbarrier = barrier(pgeo)
+        locbarrier.setlayername("locgeo")
+        self.db.barriergeo.insert(locbarrier)
+    
+    def delannobarrier(self):
+        self.db.barriergeo.remove({"layername":"locgeo"})
     
 class point:
     
--- optimizemodel.py
+++ optimizemodel.py
@@ -1,1 +1,143 @@
-﻿
+﻿'''
+    author:Octeufer
+    date:2013/11/28
+'''
+
+import json
+import pymongo
+from bson.son import SON
+import shapefile
+import math
+import AnnoOptmize
+import random
+
+dis = float(3)
+psym = float(3)
+annosymwidth = float(15)
+pi = math.pi
+#pi的位置
+scores = [0,2,3,4,4,4,1,2,4]
+
+class optimizemodel:
+    
+    def __init__(self,dbname):
+        self.client = pymongo.MongoClient("localhost",27017)
+        self.db = self.client[dbname]
+    
+    def locgenerate(self,p0,dis,psym,annosymwidth):
+        k = dis + psym / 2 + annosymwidth / 2
+        x0 = p0[0]
+        y0 = p0[1]
+        directions = []
+        for i in range(8):
+            x = x0 + k * math.cos(i * pi / 4)
+            y = y0 + k * math.sin(i * pi / 4)
+            polyanno = AnnoOptmize.rectp([x,y],3,15)
+            directions.append((polyanno,i))
+        return directions
+    
+    def domaingenerate(self,lyrname):
+        
+        if(lyrname == None):
+            for point in self.db.annopois.find():
+                direcs = dirloccheck(point)
+                self.db.annopois.update({"directions":direcs})
+                
+        else:
+            for point in self.db.annopois.find({"layername":"lyrname"}):
+                direcs = dirloccheck(point)
+                self.db.annopois.update({"directions":direcs})
+    
+    def dirloccheck(self,point):
+        directions = locgenerate(point["geometry"]["coordinates"])
+        for i in range(len(directions)):
+            geos = [geo for geo in self.db.barriergeo.find({"geometry":{"$geoWithin":{"$geometry":{  
+                                                 "type":"Polygon","coordinates":directions[i][0]}}}})]
+            if(len(geos)>0):
+                del directions[i]
+        return directions
+    
+    def domains(self):
+        annoloc=[]
+        for point in self.db.annopois.find():
+            l = len(point["directions"])
+            nums = [num for num in range(l)]
+            if l == 0:
+                annoloc.append(int(9))
+                continue
+            for nu in nums:
+                locnum = random.randint(0,len(nums)-1)
+                geos = [geo for geo in self.db.barriergeo.find({"geometry":{"$geoWithin":{"$geometry":{  
+                                                 "type":"Polygon","coordinates":point["directions"][nums[locnum]][0]}}}})]
+                if len(geos) == 0:
+                    AnnoOptmize.insertannobarrier(point["directions"][nums[locnum]][0])
+                    annoloc.append(point["directions"][nums[locnum]][1])
+                    break
+                elif len(geos) > 0:
+                    del nums[locnum]
+            if len(nums)==0:
+                annoloc.append(int(9))
+        return annoloc
+    
+    def cost(self,vec):
+        cost = 0
+        for v in vec:
+            cost = cost + scores[v]
+        return cost
+
+
+class annodirection:
+    
+    def __init__(self,directions):
+        if directions == []:
+            self.rr = []
+            self.ru = []
+            self.rb = []
+            self.uu = []
+            self.bb = []
+            self.ll = []
+            self.lu = []
+            self.lb = []
+        else:
+            self.rr = directions[0]
+            self.ru = directions[1]
+            self.rb = directions[2]
+            self.uu = directions[3]
+            self.bb = directions[4]
+            self.ll = directions[5]
+            self.lu = directions[6]
+            self.lb = directions[7]
+    
+    def setrr(self,orr):
+        self.rr = orr
+    def setru(self,oru):
+        self.ru = oru
+    def setrb(self,orb):
+        self.rb = orb
+    def setuu(self,ouu):
+        self.uu = ouu
+    def setbb(self,obb):
+        self.bb = obb
+    def setll(self,oll):
+        self.ll = oll
+    def setlu(self,olu):
+        self.lu = olu
+    def setlb(self,olb):
+        self.lb = olb
+    
+    def getrr(self):
+        return self.rr
+    def getru(self):
+        return self.ru
+    def getrb(self):
+        return self.rb
+    def getuu(self):
+        return self.uu
+    def getbb(self):
+        return self.bb
+    def getlu(self):
+        return self.lu
+    def getll(self):
+        return self.ll
+    def getlb(self):
+        return self.lb
